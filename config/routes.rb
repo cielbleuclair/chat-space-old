cfa641e645a70cc176feec3fd0f464211d6e806e
cielbleuclair <matsunagazm@gmail.com>
2019-10-06 14:24:33 +0900

    Deleted two files and the second line of "routes.rb"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages_controller/index'
   root 'messages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
